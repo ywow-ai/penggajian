9cf2702ef1d1c39f39244180d19c7e7242efc8ee
Yaqin <masyaqin37@gmail.com>
2023-01-28 23:06:01 +0700

    menambah fitur schedul

---
+++ application/controllers/Coba.php
@@ -0,0 +1,10 @@
+<?php
+
+class Coba extends CI_Controller
+{
+
+    public function index()
+    {
+        echo "wow";
+    }
+}
--- application/controllers/admin/Mail.php
+++ application/controllers/admin/Mail.php
@@ -3,29 +3,30 @@
 class Mail extends CI_Controller
 {
 
-    public function __construct()
-    {
-        parent::__construct();
+    // public function __construct()
+    // {
+    //     parent::__construct();
 
-        if ($this->session->userdata('hak_akses') != '1') {
-            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-				<strong>Anda Belum Login!</strong>
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				<span aria-hidden="true">&times;</span>
-				</button>
-				</div>');
-            redirect('login');
-        }
-    }
+    //     if ($this->session->userdata('hak_akses') != '1') {
+    //         $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    //     		<strong>Anda Belum Login!</strong>
+    //     		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    //     		<span aria-hidden="true">&times;</span>
+    //     		</button>
+    //     		</div>');
+    //         redirect('login');
+    //     }
+    //     $this->_sendEmail();
+    // }
 
     public function index()
     {
 
-        $this->load->view('template_admin/header');
-        $this->load->view('template_admin/sidebar');
+        // $this->load->view('template_admin/header');
+        // $this->load->view('template_admin/sidebar');
         $this->_sendEmail();
-        $this->load->view('admin/about');
-        $this->load->view('template_admin/footer');
+        // $this->load->view('admin/about');
+        // $this->load->view('template_admin/footer');
     }
 
     private function _sendEmail()
